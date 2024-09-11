3cc9f227b080a08df00f70839d6f3c1185edc299
Alex Gao <z5591692@ad.unsw.edu.au>
2024-09-11 23:29:10 +1000

    Adding skeleton files for the question pages

--- src/pages/questionPage.tsx
+++ src/pages/questionPage.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import jsonData from '../pages/test.json';
+import App from '..App.css'
 import './index.css'
+//import questionDetail from './src/pages/questions.tsx'
 
 // JUST IMPORT THIS INTO app.tsx and RUN ON THE ONE PAGE :))
 
@@ -14,10 +16,31 @@ interface pointAssignment {
     skeptic: number,
     longAsItWorks: number,
     aesthetics: number,
+};
+
+interface questionDetail {
+    Name: String,
+    Number: Number,
+    Answers: object,
+    Points: pointAssignment,
+};
+
+// Turns the JSON representations of questions into an object that can easily
+// be used in the quiz.
+// Outputs an array of questionDetail object members
+function questionSetup() {
+    let questions = [];
+
+    const loadData = () => JSON.parse(JSON.stringify(jsonData));
+    
+
+    return {
+
+    }
 }
 
 function Page() {
-    
+
 
     return {
 
---
+++ src/pages/questions.tsx
@@ -0,0 +1,29 @@
+import './index.css'
+
+interface pointAssignment {
+    weeb: number,
+    yapper: number,
+    sage: number,
+    atlassian: number,
+    money: number,
+    skeptic: number,
+    longAsItWorks: number,
+    aesthetics: number,
+}
+
+interface questionDetail {
+    Name: String,
+    Number: Number,
+    Answers: object,
+    Points: pointAssignment,
+}
+
+function questionSetup() {
+    
+
+    return {
+
+    }
+}
+
+export default questionDetail
