b299fcbc003a13849874de58d83335fdc530e51c
Alex Gao <z5591692@ad.unsw.edu.au>
2024-09-11 21:58:04 +1000

    added interface

--- src/pages/questionPage.tsx
+++ src/pages/questionPage.tsx
@@ -5,8 +5,19 @@ import './index.css'
 
 // JUST IMPORT THIS INTO app.tsx and RUN ON THE ONE PAGE :))
 
-function Page() {
+interface pointAssignment {
+    weeb: number,
+    yapper: number,
+    sage: number,
+    atlassian: number,
+    money: number,
+    skeptic: number,
+    longAsItWorks: number,
+    aesthetics: number,
+}
 
+function Page() {
+    
 
     return {
 
