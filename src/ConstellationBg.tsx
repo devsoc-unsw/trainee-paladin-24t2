0a6c2f82297ef4701eb773d6d270b76843a627e3
ST <159541338+DampBiscuitt@users.noreply.github.com>
2024-09-14 22:31:50 +1000

    Added component

---
+++ src/ConstellationBg.tsx
@@ -0,0 +1,103 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useState } from "react";
+// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+interface ParticlesComponentProps {
+    id: string;
+}
+
+const ParticlesComponent: React.FC<ParticlesComponentProps> = ({ id }) => {
+    const [init, setInit] = useState<boolean>(false);
+    
+    useEffect(() => {
+        initParticlesEngine(async (engine: any) => {
+            // This loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // Starting from v2 you can add only the features you need reducing the bundle size
+            // await loadAll(engine);
+            // await loadFull(engine);
+            await loadSlim(engine);
+            // await loadBasic(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = (container: any) => {
+        console.log(container);
+    };
+
+    const options = useMemo(() => ({
+        background: {
+            color: {
+                value: "#000000",
+            },
+        },
+        fpsLimit: 120,
+        interactivity: {
+            events: {
+                onClick: {
+                    enable: true,
+                    mode: "repulse",
+                },
+                onHover: {
+                    enable: true,
+                    mode: 'grab',
+                },
+            },
+            modes: {
+                push: {
+                    distance: 200,
+                    duration: 15,
+                },
+                grab: {
+                    distance: 150,
+                },
+            },
+        },
+        particles: {
+            color: {
+                value: "#FFFFFF",
+            },
+            links: {
+                color: "#FFFFFF",
+                distance: 150,
+                enable: true,
+                opacity: 0.3,
+                width: 1,
+            },
+            move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                    default: "bounce",
+                },
+                random: true,
+                speed: 1,
+                straight: false,
+            },
+            number: {
+                density: {
+                    enable: true,
+                },
+                value: 150,
+            },
+            opacity: {
+                value: 1.0,
+            },
+            shape: {
+                type: "circle",
+            },
+            size: {
+                value: { min: 1, max: 3 },
+            },
+        },
+        detectRetina: true,
+    }), []);
+
+    return <Particles id={id} init={particlesLoaded} options={options} />;
+};
+
+export default ParticlesComponent;
